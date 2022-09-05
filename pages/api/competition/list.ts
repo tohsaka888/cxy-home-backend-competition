f201a784662d39107933a8e89862b4abbfb7354c
tohsaka888 <156132264@qq.com>
2022-09-05 15:18:54 +0800

    ✨ feat: 获取比赛列表

---
+++ pages/api/competition/list.ts
@@ -0,0 +1,67 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-09-05 13:38:42
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-09-05 13:44:06
+ * @Description: 请填写简介
+ */
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { connectDB } from '@utils/connectDB'
+import Cors from 'cors'
+import { runMiddleware } from '@utils/runMiddleware'
+
+/**
+ * @openapi
+ * /api/competition/list:
+ *   get:
+ *     description: 获取比赛简单列表
+ *     responses:
+ *       200:
+ *         description: 返回列表
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success: 
+ *                   type: boolean
+ *                 list: 
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                         example: axxss
+ *                       name:
+ *                         type: string
+ *                         example: 比赛
+ *               
+ */
+
+ const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD',],
+  origin: '*',
+  preflightContinue: true
+})
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await runMiddleware(req, res, cors)
+    const db = await connectDB()
+    if (db) {
+      const competition = db.collection('competition')
+      const competitions = await competition.find({}).toArray()
+      res.status(200).json({ success: true, list: competitions })
+    } else {
+      new Error('连接数据库失败')
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: (error as Error).message })
+  }
+}
